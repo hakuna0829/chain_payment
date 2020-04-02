794338339b5c4d5b161bc211006ebeec9df00991
hakuna <hakuna0829@gmail.com>
2020-04-03 03:40:00 +0700

    add login page

--- src/components/auth/LoginPage.js
+++ src/components/auth/LoginPage.js
@@ -6,18 +6,25 @@ import {
   Checkbox,
   Radio
 } from "react-bootstrap4-form-validation";
-// import validator from 'validator'
+import validator from 'validator'
 import "../../styles/auth.scss";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isRemember, setRemember] = useState(false);
 
-  let handleChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
+  let handleEmail = e => {
+    setEmail(e.target.value)
+  }
+
+  let handlePassword = e => {
+    setPassword(e.target.value)
+  }
+
+  let handleRemember = e => {
+    setRemember(e.target.value);
+  }
 
   let handleSubmit = (e, formData, inputs) => {
     e.preventDefault();
@@ -38,18 +45,39 @@ export default function LoginPage() {
             onErrorSubmit={handleErrorSubmit}
           >
             <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <TextInput
-                name="email"
-                id="email"
-                type="email"
-                // prepend={<span className="input-group-text">@</span>}
-                successMessage="Looks good!"
-                required
-              />
+                <label htmlFor="email">Email</label>
+                <TextInput name="email" id="email" type="email" 
+                    validator={validator.isEmail} 
+                    errorMessage={{validator:"Please enter a valid email"}}
+                    value={email}
+                    onChange={handleEmail}
+                    />
+            </div>
+            <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <TextInput name="password" id="password" type="password" required 
+                    pattern="(?=.*[A-Z]).{6,}"
+                    errorMessage={{required:"Password is required", pattern: "Password should be at least 6 characters and contains at least one upper case letter"}}
+                    value={password}
+                    onChange={handlePassword}
+                />
+            </div>
+            <div className="form-group">
+                <Checkbox name="isRemember" label="Remember Me" id="isRemember" 
+                    value={isRemember}
+                    onChange={handleRemember}
+                />                
+            </div>
+            <Link to="">Forgot your password?</Link>
+            <p className="note">
+              (If there are too many login attempts, your account<br/> 
+              will be automatically locked for 60 minutes)
+            </p>
+            <div className="form-group">
+                <button className="btn btn-button-light">LOGIN</button>
             </div>
           </ValidationForm>
-          {/* <Link to="">Forgot your password?</Link> */}
+          
         </div>
         <div className="right_panel">
           <img src="images/desktop.png" alt="desktop" />
