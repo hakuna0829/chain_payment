86e54b8d1a7d2f529afc388366c4ff9c5214000f
hakuna <hakuna0829@gmail.com>
2020-04-03 20:34:03 +0700

    update login page

--- src/components/auth/LoginPage.js
+++ src/components/auth/LoginPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, createRef } from "react";
 import { Link } from "react-router-dom";
 import {
   ValidationForm,
@@ -7,12 +7,21 @@ import {
   Radio
 } from "react-bootstrap4-form-validation";
 import validator from 'validator'
+import Recaptcha from "react-recaptcha";
 import "../../styles/auth.scss";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isRemember, setRemember] = useState(false);
+  const [isVerified, setVerified] = useState(false);
+  // create our ref
+  const refEmail = useRef();
+  const refPwd = useRef();
+  const refForm = useRef();
+
+  // const formRef = createRef();
+  const refRemember = useRef();
 
   let handleEmail = e => {
     setEmail(e.target.value)
@@ -26,6 +35,10 @@ export default function LoginPage() {
     setRemember(e.target.value);
   }
 
+  let recaptchaLoaded = () => {
+    console.log('captcha loaded')
+  }
+  
   let handleSubmit = (e, formData, inputs) => {
     e.preventDefault();
     alert(JSON.stringify(formData, null, 2));
@@ -40,44 +53,58 @@ export default function LoginPage() {
         <div className="left_form">
           <img src="images/CP_logo_2.png" alt="logo" />
           <h3>Login to your account</h3>
+          
           <ValidationForm
             onSubmit={handleSubmit}
             onErrorSubmit={handleErrorSubmit}
+            ref={refForm}
           >
-            <div className="form-group">
+            {/* <div className="form-group">
                 <label htmlFor="email">Email</label>
                 <TextInput name="email" id="email" type="email" 
                     validator={validator.isEmail} 
                     errorMessage={{validator:"Please enter a valid email"}}
                     value={email}
                     onChange={handleEmail}
+                    inputref={refEmail}
                     />
-            </div>
-            <div className="form-group">
+            </div> */}
+            {/* <div className="form-group">
                 <label htmlFor="password">Password</label>
                 <TextInput name="password" id="password" type="password" required 
                     pattern="(?=.*[A-Z]).{6,}"
                     errorMessage={{required:"Password is required", pattern: "Password should be at least 6 characters and contains at least one upper case letter"}}
                     value={password}
                     onChange={handlePassword}
+                    inputref={refPwd}
                 />
             </div>
             <div className="form-group">
-                <Checkbox name="isRemember" label="Remember Me" id="isRemember" 
+                <Checkbox name="isRemember" label="Remember Me" id="isRemember"  inputref={refRemember}
                     value={isRemember}
                     onChange={handleRemember}
                 />                
-            </div>
-            <Link to="">Forgot your password?</Link>
+            </div> */}
+            {/* <Link to="">Forgot your password?</Link>
             <p className="note">
               (If there are too many login attempts, your account<br/> 
               will be automatically locked for 60 minutes)
             </p>
-            <div className="form-group">
+            <Recaptcha 
+            sitekey="6LdFNNcUAAAAAGKsWDe9Xs2Fj_d29ywe2ae4nIsF"
+            render="explicit"
+            onloadCallback={recaptchaLoaded}
+            ></Recaptcha> */}
+            {/* <div className="form-group">
                 <button className="btn btn-button-light">LOGIN</button>
-            </div>
+            </div> */}
           </ValidationForm>
           
+            <Recaptcha 
+            sitekey="6LdFNNcUAAAAAGKsWDe9Xs2Fj_d29ywe2ae4nIsF"
+            render="explicit"
+            onloadCallback={recaptchaLoaded}
+            ></Recaptcha>
         </div>
         <div className="right_panel">
           <img src="images/desktop.png" alt="desktop" />
